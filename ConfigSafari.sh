12839aaecec77bb83b8f4f98e83bccddefd6b9ad
Matt Johnson <matthew.w.johnson@gmail.com>
2016-07-15 10:03:23 -0600

    Pass URLs through the script
    This allows us to pass a variable through and set the default webpage.

--- ConfigSafari.sh
+++ ConfigSafari.sh
@@ -35,10 +35,18 @@
 #				   can only be passed if the script is triggered at login, logout, or by Self Service
 ####################################################################################################
 
+# Variables used by this script.
+webURL="http://www.du.edu"
+
+if [ "$4" != "" ] && [ "$webURL" == "" ]; then
+    webURL="$4"
+fi
+
+
 #Disable Automatically Open Safe Downloads
 defaults write /System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.Safari AutoOpenSafeDownloads -bool False
 #Set homepage
-defaults write /System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.Safari HomePage -string "http://www.du.edu"
+defaults write /System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.Safari HomePage -string $webURL
 defaults write /System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.Safari NewWindowBehavior -int 0
 defaults write /System/Library/User\ Template/Non_localized/Library/Preferences/com.apple.Safari NewTabBehavior -int 4
 #Disable Autofill Passwords
