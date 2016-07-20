81776f15c80b597626f7fb500d820ee993c394a9
Matt Johnson <matthew.w.johnson@gmail.com>
2016-07-20 14:43:34 -0600

    Initial File
    Disable Wifi

---
+++ DisableWiFi.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Created by Matt Johnson
+#Date Created: 7/20/2016
+#This script disables Wifi and prevents it from turning on without Admin privileges.
+#http://www.techrepublic.com/article/pro-tip-manage-wi-fi-with-terminal-commands-on-os-x/
+
+$airport = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+
+$airport prefs RequireAdminIBSS=YES
+$airport prefs RequireAdminPowerToggle=YES
+$airport prefs RequireAdminNetworkChange=YES
+
