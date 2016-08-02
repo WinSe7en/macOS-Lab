c7b11dfae0624fcb0dba71fdb0a07f6863d42ef2
Matt Johnson <matthew.w.johnson@gmail.com>
2016-08-02 13:41:55 -0600

    Set the wallpaper to the Milky Way wallpaper.

---
+++ EDP/EDP-SetWallpaper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ "$4" != "" ] && [ "$WallpaperName" == "" ]; then
+    WallpaperName="$4"
+fi
+
+mv /System/Library/CoreServices/DefaultDesktop.jpg /System/Library/CoreServices/DefaultDesktop.old.jpg
+ln -s "/Library/Desktop\ Pictures/Milky Way.jpg" /System/Library/CoreServices/DefaultDesktop.jpg
