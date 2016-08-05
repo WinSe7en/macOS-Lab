7f25136043a175c9dc246f46e134fc51e372a62f
Matt Johnson <matthew.w.johnson@gmail.com>
2016-08-05 08:55:21 -0600

    Update EDP-SetWallpaper.sh

--- EDP/EDP-SetWallpaper.sh
+++ EDP/EDP-SetWallpaper.sh
@@ -2,9 +2,7 @@
 
 WallpaperName="Milky Way.jpg"
 
-if [ "$4" != "" ] && [ "$WallpaperName" == "" ]; then
-    WallpaperName="$4"
-fi
-
-mv /System/Library/CoreServices/DefaultDesktop.jpg /System/Library/CoreServices/DefaultDesktop.old.jpg
-ln -s "/Library/Desktop\ Pictures/Milky Way.jpg" /System/Library/CoreServices/DefaultDesktop.jpg
+mv /Library/Desktop\ Pictures/El\ Capitan.jpg /Library/Desktop\ Pictures/El\ Capitan\ 3.jpg
+cp /Library/Desktop\ Pictures/Milky\ Way.jpg /Library/Desktop\ Pictures/El\ Capitan.jpg
+#mv /System/Library/CoreServices/DefaultDesktop.jpg /System/Library/CoreServices/DefaultDesktop.old.jpg
+#ln -s "/Library/Desktop\ Pictures/Milky Way.jpg" /System/Library/CoreServices/DefaultDesktop.jpg
