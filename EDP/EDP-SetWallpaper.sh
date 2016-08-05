982f6cc7b538dc380ddffe06eae28d5b1489518b
Matt Johnson <matthew.w.johnson@gmail.com>
2016-08-05 09:15:56 -0600

    Update EDP-SetWallpaper.sh

--- EDP/EDP-SetWallpaper.sh
+++ EDP/EDP-SetWallpaper.sh
@@ -5,7 +5,7 @@ WallpaperName="Milky Way.jpg"
 mv /Volumes/Macintosh\ HD/Library/Desktop\ Pictures/El\ Capitan.jpg /Library/Desktop\ Pictures/El\ Capitan\ 3.jpg
 cp /Volumes/Macintosh\ HD/Library/Desktop\ Pictures/Milky\ Way.jpg /Library/Desktop\ Pictures/El\ Capitan.jpg
 
-rm /Library/Caches/com.apple.desktop.admin.png 
+rm /Volumes/Macintosh\ HD/Library/Caches/com.apple.desktop.admin.png 
 
 sips -s format png /Volumes/Macintosh\ HD/Library/Desktop\ Pictures/Milky\ Way.jpg --out /Volumes/Macintosh\ HD/Library/Caches/com.apple.desktop.admin.png
 #mv /System/Library/CoreServices/DefaultDesktop.jpg /System/Library/CoreServices/DefaultDesktop.old.jpg
